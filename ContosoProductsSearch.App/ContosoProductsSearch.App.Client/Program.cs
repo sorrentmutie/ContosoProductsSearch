aa86bce025aaee39c669ec4ed244ed504a80f0b7
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-09-17 10:12:05 +0200

    Codice per menu

---
+++ ContosoProductsSearch.App/ContosoProductsSearch.App.Client/Program.cs
@@ -0,0 +1,11 @@
+using ContosoProductsSearch.App.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+await builder.Build().RunAsync();
