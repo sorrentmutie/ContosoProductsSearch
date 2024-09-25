a02514c14696de67be08a2fb4f331b8844884726
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-09-25 11:39:27 +0200

    extension method compleanno

---
+++ ContosoProductsSearch.App/ContosoProductsSearch.App/ExtensionMethods/AuthenticationExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿namespace ContosoProductsSearch.App.ExtensionMethods
+{
+    public static class AuthenticationExtensionMethods
+    {
+        public static bool IsBirthday(this AuthenticationState authenticationState, string ClaimCompleanno)
+        {
+            var customerClaimDataNascita = authenticationState.User.Claims.FirstOrDefault(c => c.Type == ClaimCompleanno);
+            if (customerClaimDataNascita is not null)
+            {
+                var dataNascita = Convert.ToDateTime(customerClaimDataNascita.Value);
+                if (DateTime.Now.Month == dataNascita.Month && DateTime.Now.Day == dataNascita.Day)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
